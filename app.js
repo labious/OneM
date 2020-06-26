9110342c958376b2b4f59ce973700a7153deddb0
Labious Phetoane <labiousphetoane@gmail.com>
2020-06-26 09:10:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+//return div content to the page 
+$(document).ready(
+    function(){
+        $('div').each(
+            function(){
+                
+            }       
+        );
+}
+);
+
+
+
+
+
+
+
+/*
+//use $(document).ready() to detect when a page haas loaded 
+$(document).ready(
+    function(){
+        $('#mainC').html('Hello Lab');
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//using JS to detect window load
+window.onload = function(){
+    console.log("Javacript detected that the window has loaded");
+}
+*/
